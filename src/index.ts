f3fb0684aef26da396e526a2355bf5dc4a9baa6a
Schuppel1 <sven.schu.mobil@gmail.com>
2022-05-03 23:47:36 +0200

    added a basic player and gravity
    Added a player square.
    Added gravity
    Modules still todo

--- src/index.ts
+++ src/index.ts
@@ -34,8 +34,8 @@ let canvas: HTMLCanvasElement = document.getElementById("canvas-game") as HTMLCa
 function setGamesize(): void {
     let h1Element = document.querySelector("h1")!;
     let h1Size: number = h1Element.clientHeight;
-    let h1MarginTop: number = parseInt(window.getComputedStyle(h1Element).marginTop) + 1;
-    let h1MarginBot: number = parseInt(window.getComputedStyle(h1Element).marginBottom) + 1;
+    let h1MarginTop: number = parseInt(getComputedStyle(h1Element).marginTop) + 1;
+    let h1MarginBot: number = parseInt(getComputedStyle(h1Element).marginBottom) + 1;
 
     canvas.height = window.innerHeight - h1Size - h1MarginTop - h1MarginBot - 5;
     canvas.width = window.innerWidth - 10;
@@ -48,6 +48,64 @@ window.addEventListener("resize", function () {
 });
 
 
-let c = canvas.getContext("2d");
+let context = canvas.getContext("2d");
+
+//Map Constante Gravitation. (Die Fallgeschwindigkeit/pysikForce.y nimmt beim fallen zu.)
+const gravitiy:number = .1;
+
+//Modul auslagern!!!
+class Player {
+    height: number;
+    width: number;
+    position: { x: number; y: number; };
+    pysikForce: { x: number; y: number; };
+
+    constructor() {
+        this.position = {
+            x: 50,  // x Position des Spielers
+            y: 50   // Y Position des Spielers
+        }
+        this.pysikForce = {
+            x: 0, // Kraft in x Richtung (könnte Wind sein)
+            y: 0  // Gravitation nach unten
+        }
+        this.width = 100  // Breite des Spielers
+        this.height = 100 // Höhe des Spielers
+    }
+
+    private draw(): void {
+        context!.fillStyle = 'red'
+        context!.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+
+    // wird nach jedem Zeitintervall aufgerufen. Updatet die Position etc. 
+    update(): void {
+        this.eraseOldFrame();
+        this.position.y += this.pysikForce.y;
+        this.position.x += this.pysikForce.x;
+        //später auslagern und überprüfen auf Platformen. 
+        if(this.position.y + this.height + this.pysikForce.y <= canvas.height) {
+            this.pysikForce.y += gravitiy;
+        } else {
+            this.pysikForce.y = 0;
+        }
+        this.draw();
+    }
+
+    private eraseOldFrame(): void {
+        // Ohne -1 bein der Y - Position gibt es Streifen. 
+        context?.clearRect(this.position.x, this.position.y-1, this.width, this.height);
+    }
+}
 
 
+
+const player: Player = new Player()
+
+
+function animate(): void {
+    requestAnimationFrame(animate)
+    player.update();
+}
+
+animate();
